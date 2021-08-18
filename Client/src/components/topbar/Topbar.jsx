3680a293c4c614893e37e37beb2c230d468428bc
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-18 14:07:12 +0530

    topbar UI Completed...

---
+++ Client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import "./Topbar.scss";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Social</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Search for whats in your mind"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Home Page</span>
+          <span className="topbarLink">Time Line</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
